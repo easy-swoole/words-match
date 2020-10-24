7280c30942d13c48fe9474b01b97f6281130f41e
huizhang001 <tuzisir@163.com>
2020-10-24 22:47:24 +0800

    Feat: 单元测试

--- src/ManagerProcess.php
+++ src/ManagerProcess.php
@@ -7,19 +7,14 @@
  */
 namespace EasySwoole\WordsMatch;
 
-use EasySwoole\Component\WaitGroup;
-use EasySwoole\EasySwoole\Trigger;
-use EasySwoole\Trigger\Location;
-use EasySwoole\WordsMatch\Config\Config;
 use Swoole\Coroutine\Socket;
-use EasySwoole\Spl\SplFileStream;
-use EasySwoole\WordsMatch\Base\Dfa;
+use EasySwoole\Trigger\Location;
+use EasySwoole\EasySwoole\Trigger;
 use EasySwoole\Component\Process\Socket\AbstractUnixProcess;
 use EasySwoole\Component\Process\Socket\UnixProcessConfig;
 use EasySwoole\WordsMatch\Extend\Protocol\Package;
 use EasySwoole\WordsMatch\Extend\Protocol\Protocol;
 use EasySwoole\WordsMatch\Config\WordsMatchConfig;
-use EasySwoole\WordsMatch\Exception\RuntimeError;
 
 class ManagerProcess extends AbstractUnixProcess {
 
@@ -28,23 +23,31 @@ class ManagerProcess extends AbstractUnixProcess {
 
     public function __construct(UnixProcessConfig $config)
     {
-        ini_set('memory_limit','4048M');
+        ini_set('memory_limit','4096M');
         $this->actionQueue = new \SplQueue();
         parent::__construct($config);
     }
 
     public function run($arg)
     {
-        $this->buildTrees();
-        $wordBanks = WordsMatchConfig::getInstance()->getWordBanks();
+        foreach (WordsMatchConfig::getInstance()->getWordBanks() as $key => $item)
+        {
+            $this->wordBanksMd5[$key] = md5_file($item);
+        }
+
+        Library::getInstance()->buildTrees();
 
         // 监听文件变化重新生成词库
-        $this->addTick(3000, function () use ($wordBanks) {
+        $this->addTick(3000, function () {
+            $wordBanks = WordsMatchConfig::getInstance()->getWordBanks();
             foreach ($this->wordBanksMd5 as $key => $item)
             {
-                if ($item !== md5_file($wordBanks[$key]))
+                $md5file = md5_file($wordBanks[$key]);
+                if ($item !== $md5file)
                 {
-                    $this->buildTrees();
+                    $this->wordBanksMd5[$key] = $md5file;
+                    Library::getInstance()->buildTrees();
+                    break;
                 }
             }
         });
@@ -60,170 +63,19 @@ class ManagerProcess extends AbstractUnixProcess {
             {
                 $commandPayload = $this->actionQueue->dequeue();
                 /** @var $fromPackage Package*/
-                $replayData = null;
                 $fromPackage = unserialize($commandPayload);
-                $wordBanks = $fromPackage->getWordBanks();
-                $wordBankFiles = WordsMatchConfig::getInstance()->getWordBanks();
-                $separator = WordsMatchConfig::getInstance()->getSeparator();
                 switch ($fromPackage->getCommand()) {
                     case $fromPackage::ACTION_REMOVE:
-                        foreach ($wordBanks as $wordBank)
-                        {
-                            $wordBankFile = $wordBankFiles[$wordBank];
-                            $splFileStream = new SplFileStream($wordBankFile, 'r');
-                            $splFileStream->lock(LOCK_EX);
-                            $content = '';
-                            while (!$splFileStream->eof())
-                            {
-                                $line = trim(fgets($splFileStream->getStreamResource()));
-                                if (empty($line)) {
-                                    continue;
-                                }
-                                $lineArr = explode($separator, $line);
-                                $word = array_shift($lineArr);
-                                if ($word === $fromPackage->getWord())
-                                {
-                                    continue;
-                                }
-                                $content .= $line.PHP_EOL;
-                            }
-                            $splFileStream->unlock(LOCK_UN);
-                            $splFileStream->close();
-                            $splFileStream = new SplFileStream($wordBankFile, 'w');
-                            $splFileStream->lock(LOCK_EX);
-                            $splFileStream->write($content);
-                            $splFileStream->unlock(LOCK_UN);
-                            $splFileStream->close();
-                        }
+                        Library::getInstance()->remove($fromPackage);
                         break;
                     case $fromPackage::ACTION_APPEND:
-                        {
-                            foreach ($wordBanks as $wordBank) {
-                                $wordBankFile = $wordBankFiles[$wordBank];
-                                $splFileStream = new SplFileStream($wordBankFile, 'a+');
-                                $splFileStream->lock(LOCK_EX);
-                                $separator = WordsMatchConfig::getInstance()->getSeparator();
-                                $row = $fromPackage->getWord();
-                                while (!$splFileStream->eof())
-                                {
-                                    $line = trim(fgets($splFileStream->getStreamResource()));
-                                    if (empty($line)) {
-                                        continue;
-                                    }
-                                    $lineArr = explode($separator, $line);
-                                    $word = array_shift($lineArr);
-                                    if ($word === $fromPackage->getWord())
-                                    {
-                                        return;
-                                    }
-                                }
-                                if (!empty($fromPackage->getOtherInfo())) {
-                                    $row .= $separator . implode($separator, $fromPackage->getOtherInfo());
-                                }
-                                $row .= PHP_EOL;
-                                $splFileStream->write($row);
-                                $splFileStream->unlock(LOCK_UN);
-                                $splFileStream->close();
-                            }
-                        }
+                        Library::getInstance()->append($fromPackage);
                         break;
                 }
             }
         });
 
         parent::run($this->getConfig());
-    }
-
-    private function buildTrees()
-    {
-        $groups = [];
-        $trees = [];
-        $wordBanks = WordsMatchConfig::getInstance()->getWordBanks();
-        $wait = new WaitGroup();
-        foreach ($wordBanks as $key => $wordBank)
-        {
-            if (file_exists($wordBank)) {
-                $wait->add();
-                go(function () use ($wait, $key, $wordBank, &$trees, &$groups) {
-                    $this->wordBanksMd5[$key] = md5_file($wordBank);
-                    [$tree, $group] = $this->buildTree($wordBank);
-                    $trees[$key] = $tree;
-                    $groups[$key] = $group;
-                    $wait->done();
-                });
-            } else {
-                throw new RuntimeError('Please set up word bank correctly！');
-            }
-        }
-        $wait->wait();
-
-        $groupsSerialize = json_encode($groups, JSON_UNESCAPED_UNICODE);
-        $splFileStream = new SplFileStream(Config::GROUPS_SERIALIZE, 'w');
-        $splFileStream->lock(LOCK_EX);
-        $splFileStream->write($groupsSerialize);
-        $splFileStream->unlock(LOCK_UN);
-        $splFileStream->close();
-
-        $treesSerialize = serialize($trees);
-        $splFileStream = new SplFileStream(Config::WORDSMATCH_SERIALIZE, 'w');
-        $splFileStream->lock(LOCK_EX);
-        $splFileStream->write(time().$treesSerialize);
-        $splFileStream->unlock(LOCK_UN);
-        $splFileStream->close();
-
-    }
-
-    private function buildTree($file)
-    {
-        $splFileStream = new SplFileStream($file, 'r');
-        $splFileStream->lock(LOCK_EX);
-        $tree = new Dfa();
-        $separator = WordsMatchConfig::getInstance()->getSeparator();
-        $normalWords = [];
-        $compoundWords = [];
-        $group = [];
-        while (!$splFileStream->eof()) {
-            $line = trim(fgets($splFileStream->getStreamResource()));
-            if (empty($line)) {
-                continue;
-            }
-            $lineArr = explode($separator, $line);
-            $first = array_shift($lineArr);
-            $words = explode(Config::COMPOUND_WORD_SEPARATOR, $first);
-            $isCompoundWord = count($words) > 1;
-            foreach ($words as $word)
-            {
-                $other = [];
-                if ($isCompoundWord)
-                {
-                    $group[$word][] = [
-                        $first,
-                        implode(',', $lineArr)
-                    ];
-                    $compoundWords[] = $word;
-                    if (array_key_exists($word, $normalWords))
-                    {
-                        $other = $normalWords[$word];
-                        $other['type'] = Config::WORD_TYPE_NORMAL_AND_COMPOUND;
-                    } else {
-                        $other['type'] = Config::WORD_TYPE_COMPOUND;
-                    }
-                } else {
-                    $normalWords[] = $word;
-                    if (in_array($word, $compoundWords, false))
-                    {
-                        $other = $lineArr;
-                        $other['type'] = Config::WORD_TYPE_NORMAL_AND_COMPOUND;
-                    } else {
-                        $other['type'] = Config::WORD_TYPE_NORMAL;
-                    }
-                }
-                $tree->append($word, $other);
-            }
-        }
-        $splFileStream->unlock(LOCK_UN);
-        $splFileStream->close();
-        return [$tree, $group];
     }
 
     public function onAccept(Socket $socket)
--- src/WordsMatchProcess.php
+++ src/WordsMatchProcess.php
@@ -7,10 +7,7 @@
  */
 namespace EasySwoole\WordsMatch;
 
-use EasySwoole\WordsMatch\Config\Config;
 use Swoole\Coroutine\Socket;
-use EasySwoole\Spl\SplFileStream;
-use EasySwoole\WordsMatch\Config\WordsMatchConfig;
 use EasySwoole\WordsMatch\Exception\RuntimeError;
 use EasySwoole\WordsMatch\Extend\Protocol\Package;
 use EasySwoole\WordsMatch\Extend\Protocol\Protocol;
@@ -18,8 +15,6 @@ use EasySwoole\Component\Process\Socket\AbstractUnixProcess;
 
 class WordsMatchProcess extends AbstractUnixProcess
 {
-
-    private $uuid;
 
     private $cache = [
         'trees' => [],
@@ -37,30 +32,12 @@ class WordsMatchProcess extends AbstractUnixProcess
     {
         ini_set('memory_limit',$this->getConfig()->getMaxMem().'M');
         $this->addTick(3000, function () {
-            if (!file_exists(Config::WORDSMATCH_SERIALIZE))
+            $cache = Library::getInstance()->unserializeCache();
+            if (empty($cache))
             {
                 return;
             }
-            $splFileStream = new SplFileStream(Config::WORDSMATCH_SERIALIZE, 'a+');
-            $splFileStream->lock(LOCK_EX);
-            $uuid = $splFileStream->read(10);
-            if (!is_numeric($uuid))
-            {
-                $splFileStream->unlock(LOCK_UN);
-                return;
-            }
-            if ($this->uuid !== $uuid)
-            {
-                $trees = $splFileStream->getContents();
-                $cache['trees'] = unserialize($trees);
-                $groupFileStream = new SplFileStream(Config::GROUPS_SERIALIZE, 'a+');
-                $groupFileStream->lock(LOCK_EX);
-                $groups = $groupFileStream->getContents();
-                $cache['groups'] = json_decode($groups, true);
-                $splFileStream->unlock(LOCK_UN);
-                $this->cache = $cache;
-            }
-            $splFileStream->unlock(LOCK_UN);
+            $this->cache = $cache;
         });
         parent::run($this->getConfig());
     }
@@ -92,79 +69,11 @@ class WordsMatchProcess extends AbstractUnixProcess
         switch ($fromPackage->getCommand()) {
             case $fromPackage::ACTION_SEARCH:
                 {
-                    $replayData = [];
-                    $content = $fromPackage->getContent();
-                    $wordBanks = $fromPackage->getWordBanks();
-                    if (empty($wordBanks))
-                    {
-                        $wordBanks = array_keys(WordsMatchConfig::getInstance()->getWordBanks());
-                    }
                     if (empty($this->cache['trees']))
                     {
                         break;
                     }
-                    foreach ($wordBanks as $wordBank) {
-
-                        if (!isset($this->cache['trees'][$wordBank])) {
-                            continue;
-                        }
-
-                        $result = $this->cache['trees'][$wordBank]->search($content);
-
-                        $groups = [];
-                        foreach ($result as $key => $item) {
-                            $word = $item['word'];
-                            $type = $item['other']['type'];
-                            if (in_array($type, [Config::WORD_TYPE_COMPOUND, Config::WORD_TYPE_NORMAL_AND_COMPOUND]) && isset($this->cache['groups'][$wordBank][$word])) {
-                                $compoundWords = $this->cache['groups'][$wordBank][$word];
-                                foreach ($compoundWords as $compoundWord)
-                                {
-                                    $compoundWordArr = explode(Config::COMPOUND_WORD_SEPARATOR, $compoundWord[0]);
-                                    $groups[md5(sort($compoundWordArr, SORT_STRING))] = [
-                                        'compound_word' => $compoundWord[0],
-                                        'compound_word_arr' => $compoundWordArr,
-                                        'other' => explode(WordsMatchConfig::getInstance()->getSeparator(), $compoundWord[1]),
-                                        'total' => count($compoundWordArr),
-                                        'current' => 0,
-                                        'location' => []
-                                    ];
-                                }
-                            }
-                        }
-
-                        foreach ($result as $key => $item) {
-                            $word = $item['word'];
-                            $type = $item['other']['type'];
-                            unset($item['other']['type']);
-                            if ($type === Config::WORD_TYPE_NORMAL) {
-                                $item['type'] = Config::WORD_TYPE_NORMAL;
-                                $replayData[$key] = $item;
-                                continue;
-                            }
-
-                            if ($type === Config::WORD_TYPE_NORMAL_AND_COMPOUND) {
-                                $item['type'] = Config::WORD_TYPE_NORMAL;
-                                $replayData[$key] = $item;
-                            }
-
-                            if (in_array($type, [Config::WORD_TYPE_COMPOUND, Config::WORD_TYPE_NORMAL_AND_COMPOUND])) {
-                                foreach ($groups as &$compound) {
-                                    if (in_array($word, $compound['compound_word_arr'], false)) {
-                                        $compound['current'] += 1;
-                                        $compound['location'][] = $item['location'];
-                                        if ($compound['total'] === $compound['current']) {
-                                            $replayData[] = [
-                                                'word' => $compound['compound_word'],
-                                                'other' => $compound['other'],
-                                                'location' => array_merge(...$compound['location']),
-                                                'type' => Config::WORD_TYPE_COMPOUND
-                                            ];
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    $replayData = Library::getInstance()->detect($fromPackage, $this->cache);
                 }
                 break;
         }
