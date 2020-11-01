c36f48c924816bb823bfaa3a611bd1fa43605f98
huizhang001 <tuzisir@163.com>
2020-11-01 18:08:56 +0800

    Feat: 优化返回结果

--- tests/DictionaryTest.php
+++ tests/DictionaryTest.php
@@ -27,7 +27,7 @@ class DictionaryTest extends TestCase
                 'type' => 1
             ]
         ], JSON_UNESCAPED_UNICODE);
-        $this->assertEquals($expected, json_encode($res, JSON_UNESCAPED_UNICODE));
+        $this->assertEquals($expected, json_encode(array_values($res), JSON_UNESCAPED_UNICODE));
     }
 
     public function testDetectCompound()
@@ -43,7 +43,7 @@ class DictionaryTest extends TestCase
                 'type' => 2
             ],
         ], JSON_UNESCAPED_UNICODE);
-        $this->assertEquals($expected, json_encode($res, JSON_UNESCAPED_UNICODE));
+        $this->assertEquals($expected, json_encode(array_values($res), JSON_UNESCAPED_UNICODE));
     }
 
     public function testDetectNormalAndCompound()
@@ -66,7 +66,7 @@ class DictionaryTest extends TestCase
                 'type' => 2
             ],
         ], JSON_UNESCAPED_UNICODE);
-        $this->assertEquals($expected, json_encode($res, JSON_UNESCAPED_UNICODE));
+        $this->assertEquals($expected, json_encode(array_values($res), JSON_UNESCAPED_UNICODE));
     }
 
     public function testAppend()
@@ -83,7 +83,7 @@ class DictionaryTest extends TestCase
                 'type' => 1
             ]
         ], JSON_UNESCAPED_UNICODE);
-        $this->assertEquals($expected, json_encode($res, JSON_UNESCAPED_UNICODE));
+        $this->assertEquals($expected, json_encode(array_values($res), JSON_UNESCAPED_UNICODE));
     }
 
     public function testRemove()
@@ -92,7 +92,7 @@ class DictionaryTest extends TestCase
         $dictionary->remove('ES');
         $res = $dictionary->detect('测试ES');
         $expected = json_encode([]);
-        $this->assertEquals($expected, json_encode($res, JSON_UNESCAPED_UNICODE));
+        $this->assertEquals($expected, json_encode(array_values($res), JSON_UNESCAPED_UNICODE));
     }
 
 
